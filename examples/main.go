f4398d2a227f67ca7a971ae7df22f11f1a48c5e4
umut <umut.bsl@yandex.com>
2024-01-31 18:29:02 +0300

    Refactor exampleScenario function and fix image name in main.go

--- examples/main.go
+++ examples/main.go
@@ -50,7 +50,7 @@ func IsRunning(container *Container) bool {
 	return r == 1
 }
 
-func randomrunningstatus() {
+func exampleScenario(image string) {
 	deployed := map[string]Container{}
 	defer fmt.Printf("Deployed containers: %+v\n", deployed)
 
@@ -59,42 +59,7 @@ func randomrunningstatus() {
 
 	c := &Container{
 		Name:   "nginx",
-		Image:  "nginx:latest",
-		Status: ContainerStatusPending,
-	}
-
-	// Handles its own rollback if return error, so does not need to be added to rollbacker on its error, or before execution
-	rbfn, err := DeployContainer(c)
-	if err != nil {
-		return
-	}
-	// so we add it rollback function after execution
-	// when IsRunning(c) is false, we call rollback function
-	rb.Add(rbfn)
-
-	if !IsRunning(c) {
-		rb.Add(func() {
-			fmt.Printf("delete container %s\n", c.Name)
-			delete(deployed, c.Name)
-		})
-
-		return
-	}
-
-	rb.Finalize()
-	deployed[c.Name] = *c
-}
-
-func invalidimage() {
-	deployed := map[string]Container{}
-	defer fmt.Printf("Deployed containers: %+v\n", deployed)
-
-	rb := rollback.New()
-	defer rb.Rollback()
-
-	c := &Container{
-		Name:   "nginx",
-		Image:  "nginxx:latest",
+		Image:  image,
 		Status: ContainerStatusPending,
 	}
 
@@ -122,8 +87,18 @@ func invalidimage() {
 
 func main() {
 	fmt.Println("rollback example")
-	randomrunningstatus()
+	exampleScenario("nginx:latest")
+	// Image name is correct
+	// so rollback container function inside DeployContainer function will not be executed first but return the rollback function
+	// rollback func added in to upper rollbacker
+	// If isRunning is false, two rollback functions will be executed in this order
+	// - 1. delete container from deployed map
+	// - 2. rollback container function inside DeployContainer function
+	// If isRunning is true none of the rollback functions will be executed
 	fmt.Println("\n\ninvalid image example")
-	invalidimage()
-
+	exampleScenario("nginxx:latest")
+	// Image name is incorrect
+	// so rollback container function inside DeployContainer function will be executed
+	// than return nil, err (doesnt return rollback func its already executed)
+	// program will exit on err
 }
